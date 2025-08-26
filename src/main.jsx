d2f9705c935854df5b02d44e48f17dca95bbb1b0
sukhmanpreet536 <preetbadhan987@gmail.com>
2025-08-26 15:05:54 +0530

    f

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,14 @@ import App from './App.jsx'
 
 console.log("Auth0 Domain:", import.meta.env.VITE_APP_AUTH0_DOMAIN);
 console.log("Auth0 Client ID:", import.meta.env.VITE_APP_AUTH0_CLIENT_ID);
-console.log("Auth0 Redirect URI:", import.meta.env.VITE_AUTH0_REDIRECT_URI);
+
 
 createRoot(document.getElementById('root')).render(
 <Auth0Provider
   domain={import.meta.env.VITE_AUTH0_DOMAIN}
   clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
   authorizationParams={{
-    redirect_uri: import.meta.env.VITE_AUTH0_REDIRECT_URI || window.location.origin
+    redirect_uri:  window.location.origin
   }}
 >
   <App />
