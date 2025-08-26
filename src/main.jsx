cc131e13a778abf43556991b00bb167b9fc83316
sukhmanpreet536 <preetbadhan987@gmail.com>
2025-08-26 15:04:50 +0530

    f

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
 import App from './App.jsx'
 
-console.log("Auth0 Domain:", import.meta.env.VITE_AUTH0_DOMAIN);
-console.log("Auth0 Client ID:", import.meta.env.VITE_AUTH0_CLIENT_ID);
+console.log("Auth0 Domain:", import.meta.env.VITE_APP_AUTH0_DOMAIN);
+console.log("Auth0 Client ID:", import.meta.env.VITE_APP_AUTH0_CLIENT_ID);
 console.log("Auth0 Redirect URI:", import.meta.env.VITE_AUTH0_REDIRECT_URI);
 
 createRoot(document.getElementById('root')).render(
